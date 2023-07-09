0e2a4b169b3c7d8140a6335257b240a2141ea6aa
Jonathan Schmidt <jonathan.schmidt@tum.de>
2023-07-09 15:02:09 +0200

    Add backround masking

--- dataset/stable_diffusion.py
+++ dataset/stable_diffusion.py
@@ -1,5 +1,7 @@
 import os
+from typing import List
 
+import numpy as np
 import torch
 import torchvision
 from pathlib import Path
@@ -18,7 +20,7 @@ class StableDiffusionShapeNetDataset(torch.utils.data.Dataset):
         else:
             shape_dirs = [p for p in Path(root_dir).iterdir() if p.is_dir()]
         
-        self.img_paths = []
+        self.img_paths: List[Path] = []
         for shape_dir in shape_dirs:
             img_folder = shape_dir / "images"
             self.img_paths += [
@@ -36,10 +38,23 @@ class StableDiffusionShapeNetDataset(torch.utils.data.Dataset):
             ]
         )
 
+
     def __len__(self):
         return self.size
 
     def __getitem__(self, idx):
         img_path = self.img_paths[idx]
+        mask_path = img_path.parent.parent / "masks" / img_path.name
         img = Image.open(img_path)
+        mask = Image.open(mask_path)
+        background = Image.new("RGB", img.size, (255, 255, 255))
+        img = Image.composite(img, background, mask)
+
+        img.save("test.png")
         return {"image": self.transform(img) * 2 - 1}
+
+
+if __name__ == "__main__":
+    ds = StableDiffusionShapeNetDataset("/storage/user/schmidjo/shapenet-sd-photo-v2/02958343", 512)
+    batch = ds[0]
+    print(type(batch))
